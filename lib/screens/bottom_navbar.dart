4e4c326223567e39439a086cac420a03c7b3dfe9
Squisu <115937792+Squisu@users.noreply.github.com>
2026-08-07 19:45:57 +0800

    Change FAB to circle

--- lib/screens/bottom_navbar.dart
+++ lib/screens/bottom_navbar.dart
@@ -57,7 +57,8 @@ final List<Widget> tabs = [
         ]
         ),
           floatingActionButton: FloatingActionButton(
-          onPressed: () => {print("Chatbot button pressed")}, // _openChatbot(context),
+          onPressed: () => {print("Chatbot button pressed")},
+          shape: const CircleBorder(), // _openChatbot(context),
           backgroundColor: const Color(0xFF0052FF),
           foregroundColor: Colors.white,
           child: Icon(Icons.chat_bubble_outline_rounded),
